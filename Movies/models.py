8e7a8494dd928fdbbadd1ca4fc4ae5e840b9d081
jalil <95492487+leonardo0231@users.noreply.github.com>
2025-05-21 13:23:55 +0430

    add models for User and Movies

--- Movies/models.py
+++ Movies/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from Users.models import Profile
 
-# Create your models here.
+
+
+class Movies(models.Model):
+    movie_id = models.IntegerField(unique=True)
+    title = models.CharField(max_length=255)
+    genres = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+    
+class Rating(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movies, on_delete=models.CASCADE)
+    rating = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'movie')
+
--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="profile")
+    avatar = models.ImageField(upload_to="user_profile/%Y%m%d/", blank=True, null=True)
+
+    def __str__(self):
+        return f"user: {self.user.username}"
+
